e2d7c893ea4a574756be550abbb4304e5f7ab197
fagnerviana <fagsantiago@gmail.com>
2022-01-19 13:47:46 -0300

    Inicialiando o projeto

---
+++ TQI_Java_Developer/ConfigurandoBeanSpring/src/main/java/com/conigurandobeans/configurandobeans/ConfigurandobeansApplication.java
@@ -0,0 +1,13 @@
+package com.conigurandobeans.configurandobeans;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigurandobeansApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigurandobeansApplication.class, args);
+	}
+
+}
