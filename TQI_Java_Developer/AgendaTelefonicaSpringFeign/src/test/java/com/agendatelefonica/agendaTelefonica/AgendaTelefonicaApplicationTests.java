67d71a272b052acbf7b0f7c96e823a7c178bc9ed
fagnerviana <fagsantiago@gmail.com>
2022-01-24 12:43:27 -0300

    Finalizado configuração da primeira pasta

---
+++ TQI_Java_Developer/AgendaTelefonicaSpringFeign/src/test/java/com/agendatelefonica/agendaTelefonica/AgendaTelefonicaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agendatelefonica.agendaTelefonica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendaTelefonicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
