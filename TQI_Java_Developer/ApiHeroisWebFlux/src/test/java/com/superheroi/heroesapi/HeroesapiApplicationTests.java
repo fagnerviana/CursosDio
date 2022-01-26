310bed319536ed1c9019b59b1f72acab6f34ce65
fagnerviana <fagsantiago@gmail.com>
2022-01-25 21:45:01 -0300

    Baixado arquivos do Start spring io

---
+++ TQI_Java_Developer/ApiHeroisWebFlux/src/test/java/com/superheroi/heroesapi/HeroesapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.superheroi.heroesapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeroesapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
