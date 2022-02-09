ed704eaa69fd623bec55d062c30ee40518677848
fagnerviana <fagsantiago@gmail.com>
2022-02-08 21:51:08 -0300

    Criando os Pacotes:config;model;repository

---
+++ TQI_Java_Developer/ProductCatolog/src/main/java/com/example/productcatalog/repository/ProductRepository.java
@@ -0,0 +1,10 @@
+package com.example.productcatalog.repository;
+
+import com.example.productcatalog.model.Product;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ProductRepository extends CrudRepository<Product, Integer> {
+
+
+
+}
